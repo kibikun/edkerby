9fbb8d3118387c457b24f3348358aafa926fda21
PALOGA\edker <edkerbypaloga2018gmail.com>
2026-03-24 15:55:43 +0800

    Set up contact form submission via Formspree

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector('.nav__burger');
 const profile = document.querySelector('.modal__overlay');
 const closeBtn = document.getElementById('cls-btn');
-
+const form = document.getElementById("contact__form");
 btn.addEventListener('click', () => {
     profile.classList.toggle('pop');
 
@@ -10,3 +10,9 @@ btn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     profile.classList.add('pop');
 });
+
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName('form')) {
+        form.reset();
+    }
+}
